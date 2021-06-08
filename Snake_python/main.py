7e3cd13bbe24e65c5615564d4045524f83faab36
MrzAtn <51358484+MrzAtn@users.noreply.github.com>
2021-06-08 22:03:58 +0200

    Delete Snake_python directory

--- Snake_python/main.py
+++
@@ -1,8 +0,0 @@
-from interface import Interface
-
-import matplotlib.pyplot as plot
-import math
-
-if __name__ == "__main__":
-    open_I = Interface()
-    open_I.myInterface()
--- Snake_python/snake.py
+++
@@ -1,9 +0,0 @@
-'''
-@CopyRight: Come down Bro
-@Description: fileContent
-@Author: Antonin Marzelle
-@Date: 2019-08-04 15:55:15
-'''
-
-
-
